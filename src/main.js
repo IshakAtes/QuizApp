b35df0345fd51e9b7ca0d00ea124c68ab962cfc2
IshakAtes <ishakfeuer@gmail.com>
2022-07-08 00:42:06 +0200

    added Json Array

---
+++ src/main.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question": "Wer hat Html erfunden",
+        "answer1": "Robbie Williams",
+        "answer2": "Lady Gaga",
+        "answer3": "Tim Berners-Lee",
+        "answer4": "Justin Bieber",
+        "rightAnswer": 3
+    },
+    {
+        "question": "Was bedeutet das Html Tag &lt;e&gt;?",
+        "answer1": "Text Fett",
+        "answer2": "Container",
+        "answer3": "Ein Link",
+        "answer4": "Kursiv",
+        "rightAnswer": 3
+    },
+    {
+        "question": "Wie bindet man eine Website in eine Website ein?",
+        "answer1": "Robbie Williams",
+        "answer2": "Kein Plan",
+        "answer3": "Tim Berners-Lee",
+        "answer4": "Justin Bieber",
+        "rightAnswer": 2
+    },
+    {
+        "question": "Wie definiert man in JavaScript eine Variable?",
+        "answer1": "let 100 = rate;",
+        "answer2": "100 = let rate;",
+        "answer3": "rate = 100;",
+        "answer4": "let rate = 100;",
+        "rightAnswer": 4
+    },
+    {
+        "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
+        "answer1": "readonly",
+        "answer2": "max",
+        "answer3": "from",
+        "answer4": "spellcheck",
+        "rightAnswer": 1
+    },
+];
